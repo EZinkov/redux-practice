f4e0ac3a26f609ead83ffd11c8d3f2d805592cc8
Evgeniy <evgeniyzinkov98@gmail.com>
2023-03-23 13:24:38 +0100

    add: Modal, total price

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { useEffect } from "react"
 import Navbar from "./components/Navbar"
 import CartContainer from "./components/CartContainer"
+import { useSelector, useDispatch } from "react-redux"
+import { calculateTotals } from "./redux/cartSlice"
+import Modal from "./components/Modal"
 
 function App() {
+  const cartItems = useSelector((store) => store.cart)
+  const { isOpen } = useSelector((store) => store.modal)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(calculateTotals())
+  }, [cartItems])
   return (
     <>
+      {isOpen && <Modal />}
       <Navbar />
       <CartContainer />
     </>
